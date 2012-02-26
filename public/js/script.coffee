d1794dc1e03bff0717419205dc2704308decf9d7
Juan Pinilla <dark@shamain.com.ar>
2012-02-26 17:09:23 -0500

    It works! :D

--- public/js/script.coffee
+++ public/js/script.coffee
@@ -1,33 +1,148 @@
-node = {
-	init: ()->
-		$('#text').on 'change keydown click', ->
-			self = $('#text')
-			str = self.val()
-			ascii = new Array(str.length)
-			bin = new Array(str.length)
-			for i in str.length
-				ascii[i]=str.charCodeAt(i)
-				bin[i]=toBinary(ascii[i])
-			console.log(bin)
 
-	toBinary: ()->
-		answer = new Array(8);
-		for i in answer.length
-			answer[i]=0
-		x2 = decimal;
-		log2 = 0
-		while x2 >= 2
-				x2 = x2 / 2
-				log2 = log2 + 1
-		for l2 in [log2..0]
-				power = Math.pow(2, l2)
-				if decimal >= power
-						answer[l2] = 1
-						decimal -= power
-				else
-					answer[l2] = 0
-		answer.reverse()
+# namespace for static values
+dom = {
+
+	# dom objects
+	txt_char: $ '#text'
+	txt_armn: $ '#arm'
+	txt_frec: $ '#frec'
+	chk_debg: $ '#check'
+	box_debg: $ '.debug'
+	ico_load: $ '.load'
+	inputs: $ '	input'
+	graph: $ '#graph'
+	numbers: $ '.number'
+	#debug fields
+	dbg_char: $ '#dbg_char'
+	dbg_frec: $ '#dbg_frec'
+	dbg_arm: $ '#dbg_arm'
+	dbg_a0: $ '#dbg_a0'
+	dbg_an: $ '#dbg_an'
+	dbg_bn: $ '#dbg_bn'
+	dbg_ascii: $ '#dbg_ascii'
+	dbg_bin: $ '#dbg_bin'
+	dbg_bn: $ '#dbg_bn'
+	dbg_w0: $ '#dbg_w0'
+	dbg_t: $ '#dbg_t'	
 }
 
-$->
+# values
+val = {
+	pi: Math.PI
+	pi_4: Math.PI/4
+	f: 0
+	w0: 0
+	T: 0
+}
+
+# valide functions
+valide = {
+	numeric: (event)->
+		if event.keyCode is 46 or event.keyCode is 8 or event.keyCode is 9 or event.keyCode is 27 or (event.keyCode is 65 and event.ctrlKey is true) or (event.keyCode >= 35 and event.keyCode <= 39) or event.keyCode is 110
+			return true
+		else if (event.keyCode < 48 or event.keyCode > 57) and (event.keyCode < 96 or event.keyCode > 105)
+			event.preventDefault()
+}
+
+# namespace
+node = {
+
+	# init function
+	init: ()->
+		self = this
+		dom.numbers.on 'keydown', (event)->
+			valide.numeric event
+		dom.txt_char.on 'keyup', ()->
+			self._change()
+		dom.txt_armn.on 'keyup', ()->
+			self._change()
+		dom.txt_frec.on 'keyup', ()->
+			self._change()
+		dom.chk_debg.on 'click', ()->
+			if dom.chk_debg.is ':checked'
+				dom.box_debg.show 'slow'
+			else
+				dom.box_debg.hide 'slow'
+
+		# enable all fields (inputs)
+		dom.inputs.prop 'disabled', false
+		dom.ico_load.remove()
+		$('header, div, footer').removeClass 'hide'
+
+	# change for text, here is the magic :)
+	_change: ()->
+		self = this
+		char = dom.txt_char.val()
+		armn = dom.txt_armn.val()
+		frec = dom.txt_frec.val()
+		if char.length > 0 and armn.length > 0 and frec.length > 0
+			val.f = frec
+			val.w0 = 2 * val.pi * parseInt frec
+			val.T = 1 / parseInt frec * 8
+			ascii = self._toascii char
+			bin = self._tobin ascii
+			a0 = self._geta0 bin
+			self._fourier a0, bin, armn
+			dom.dbg_char.text char
+			dom.dbg_bin.text bin
+			dom.dbg_ascii.text ascii
+			dom.dbg_frec.text frec
+			dom.dbg_a0.text a0
+			dom.dbg_arm.text armn
+			dom.dbg_w0.text val.w0
+			dom.dbg_t.text val.T						
+
+	# converts character value to ascii
+	_toascii: (char)->
+		char.charCodeAt(0)
+
+	# converts decimal value to binary
+	_tobin: (decimal)->
+		bin = parseInt(decimal).toString 2
+		while bin.length < 8
+			bin = "0".concat bin
+		bin
+	
+	# returns a0 (fourier)
+	_geta0: (bin)->
+		a0 = 0
+		for i in [0..7]
+			if bin[i] == '1'
+				a0 += 1/8
+		a0
+
+	# returns an (fourier)
+	_getan: (bin, armn)->
+		an = 0
+		n = armn
+		for i in [0..7]
+			if bin[i] == '1'
+				an += ( Math.sin( n*val.pi_4*(i+1) ) - Math.sin( n*val.pi_4*i) )
+		an *= (1 / (n * val.pi))
+		
+	# returns bn (fourier)
+	_getbn: (bin, armn)->
+		bn = 0
+		n = armn
+		for i in [0..7]
+			if bin[i] == '1'
+				bn += ( Math.cos( n*val.pi_4*i ) - Math.cos( n*val.pi_4*(i+1) ) )
+		bn *= (1 / (n * val.pi))
+
+	# calcule fourier serie
+	_fourier: (a0, bin, armn)->
+		self = this
+		points = []
+		fn = (t)->
+			f = 0
+			for n in [1..armn]
+				f += self._getan(bin, n)*Math.cos(val.w0*n*t) + self._getbn(bin, n)*Math.sin(val.w0*n*t)
+			f += a0
+		# values for plot
+		for x in [0..val.T*8] by val.T/50
+			points.push([x, fn(x)])
+		$.plot(dom.graph, [points])
+}
+
+$ ()->
 	node.init()
--- public/js/script.js
+++ public/js/script.js
@@ -1,104 +1,160 @@
-var node = {
-	text: $('#text'),
-	arm: $('#arm'),
-	frec: $('#frec'),
-	init: function () {
-		var self=this;
-		self.text.on('keyup keydown', function () { node._onChange(self.text, $('#len'), $('#data')); });
-		$('.number').on('keydown', function (event) { valide.numeric(event) });
-		$('#check').on('click', function () {
-			if ($('#check:checked').length) $('.debug').show('slow');
-			else $('.debug').hide('slow');
-		});
-		self.arm.on('keyup keydown change', function () { 
-			$('.arm').text(self.arm.val());
-			node._graph();
-		});
-		self.frec.on('keyup keydown change', function () { 
-			node._graph();
-		});
+(function() {
+  var dom, node, val, valide;
 
-		//Enable all fields (inputs)
-		$('input').prop('disabled', false);
+  dom = {
+    txt_char: $('#text'),
+    txt_armn: $('#arm'),
+    txt_frec: $('#frec'),
+    chk_debg: $('#check'),
+    box_debg: $('.debug'),
+    ico_load: $('.load'),
+    inputs: $('	input'),
+    graph: $('#graph'),
+    numbers: $('.number'),
+    dbg_char: $('#dbg_char'),
+    dbg_frec: $('#dbg_frec'),
+    dbg_arm: $('#dbg_arm'),
+    dbg_a0: $('#dbg_a0'),
+    dbg_an: $('#dbg_an'),
+    dbg_bn: $('#dbg_bn'),
+    dbg_ascii: $('#dbg_ascii'),
+    dbg_bin: $('#dbg_bin'),
+    dbg_bn: $('#dbg_bn'),
+    dbg_w0: $('#dbg_w0'),
+    dbg_t: $('#dbg_t')
+  };
 
-		$('.load').remove();
-		$('header, div, footer').removeClass('hide');
+  val = {
+    pi: Math.PI,
+    pi_4: Math.PI / 4,
+    f: 0,
+    w0: 0,
+    T: 0
+  };
 
-		//test graph
-		node._graph();
-	},
-	_onChange: function (self, len, data) {
-		var str = self.val();
-		var ascii = new Array(str.length);
-		var bin = new Array(str.length);
-		var r = new Array(), j = -1;
-		len.text(str.length);
-		$.each(str, function (i, val) {
-			ascii[i] = str.charCodeAt(i);
-			bin[i] = node._toBinary(ascii[i]);
-			r[++j] ='<tr><td>';
-			r[++j] = val;
-			r[++j] = '</td><td>';
-			r[++j] = ascii[i];
-			r[++j] = '</td><td>';
-			r[++j] = bin[i].toString().split(',').join('');
-			r[++j] = '</td><td>';
-			r[++j] = 'a0';
-			r[++j] = '</td><td>';
-			r[++j] = 'an';
-			r[++j] = '</td><td>';
-			r[++j] = 'bn';
-			r[++j] = '</td></tr>';
-			data.html(r.join('')); 
-		});
-		if (str.length === 0) data.html(''); 
-		node._graph();
-		//console.log(bin);
-	},
-	_toBinary: function (decimal) {
-		answer = new Array(8);
-		for (var i = 0; i < answer.length; i++) answer[i] = 0;
-		x2 = decimal;
-		log2 = 0;
-		while (x2 >= 2) {
-			x2 = x2 / 2;
-			log2 = log2 + 1;
-		}
-		for (l2 = log2; l2 >= 0; l2--) {
-			power = Math.pow(2, l2);
-			if (decimal >= power) {
-				answer[l2] = 1;
-				decimal -= power;
-			} else answer[l2] = 0;
-		}
-		return answer.reverse();
-	},
-	_graph: function () {
-		var self=this;
-		var d1 = [];
-		var fn = function (t) {
-			var ans = 0;
-			return ans + self.frec.val()*sum(self.arm.val(), t);
-		}
+  valide = {
+    numeric: function(event) {
+      if (event.keyCode === 46 || event.keyCode === 8 || event.keyCode === 9 || event.keyCode === 27 || (event.keyCode === 65 && event.ctrlKey === true) || (event.keyCode >= 35 && event.keyCode <= 39) || event.keyCode === 110) {
+        return true;
+      } else if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) {
+        return event.preventDefault();
+      }
+    }
+  };
 
-		var sum = function (n, t) {
-			var ans = 0;
-			for (var i=1; i<=n*2; i+=2)	ans += (1/i)*Math.sin(i*t);
-			return ans;
-		}
-		if(self.arm.val().length > 0 && self.frec.val().length > 0) for (var t = 0; t < self.text.val().length*(8*Math.PI); t += 0.015) d1.push([t, fn(t)]);
-		var graph = $("#graph");
-		var plot = $.plot(graph, [d1]);	
-	}
-}
-var valide = {
-	numeric: function (event) {
-		if (event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || (event.keyCode == 65 && event.ctrlKey === true) ||
-		(event.keyCode >= 35 && event.keyCode <= 39) || event.keyCode == 110) return;
-		else if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105)) event.preventDefault();
-	}
-}
+  node = {
+    init: function() {
+      var self;
+      self = this;
+      dom.numbers.on('keydown', function(event) {
+        return valide.numeric(event);
+      });
+      dom.txt_char.on('keyup', function() {
+        return self._change();
+      });
+      dom.txt_armn.on('keyup', function() {
+        return self._change();
+      });
+      dom.txt_frec.on('keyup', function() {
+        return self._change();
+      });
+      dom.chk_debg.on('click', function() {
+        if (dom.chk_debg.is(':checked')) {
+          return dom.box_debg.show('slow');
+        } else {
+          return dom.box_debg.hide('slow');
+        }
+      });
+      dom.inputs.prop('disabled', false);
+      dom.ico_load.remove();
+      return $('header, div, footer').removeClass('hide');
+    },
+    _change: function() {
+      var a0, armn, ascii, bin, char, frec, self;
+      self = this;
+      char = dom.txt_char.val();
+      armn = dom.txt_armn.val();
+      frec = dom.txt_frec.val();
+      if (char.length > 0 && armn.length > 0 && frec.length > 0) {
+        val.f = frec;
+        val.w0 = 2 * val.pi * parseInt(frec);
+        val.T = 1 / parseInt(frec * 8);
+        ascii = self._toascii(char);
+        bin = self._tobin(ascii);
+        a0 = self._geta0(bin);
+        self._fourier(a0, bin, armn);
+        dom.dbg_char.text(char);
+        dom.dbg_bin.text(bin);
+        dom.dbg_ascii.text(ascii);
+        dom.dbg_frec.text(frec);
+        dom.dbg_a0.text(a0);
+        dom.dbg_arm.text(armn);
+        dom.dbg_w0.text(val.w0);
+        return dom.dbg_t.text(val.T);
+      }
+    },
+    _toascii: function(char) {
+      return char.charCodeAt(0);
+    },
+    _tobin: function(decimal) {
+      var bin;
+      bin = parseInt(decimal).toString(2);
+      while (bin.length < 8) {
+        bin = "0".concat(bin);
+      }
+      return bin;
+    },
+    _geta0: function(bin) {
+      var a0, i;
+      a0 = 0;
+      for (i = 0; i <= 7; i++) {
+        if (bin[i] === '1') a0 += 1 / 8;
+      }
+      return a0;
+    },
+    _getan: function(bin, armn) {
+      var an, i, n;
+      an = 0;
+      n = armn;
+      for (i = 0; i <= 7; i++) {
+        if (bin[i] === '1') {
+          an += Math.sin(n * val.pi_4 * (i + 1)) - Math.sin(n * val.pi_4 * i);
+        }
+      }
+      return an *= 1 / (n * val.pi);
+    },
+    _getbn: function(bin, armn) {
+      var bn, i, n;
+      bn = 0;
+      n = armn;
+      for (i = 0; i <= 7; i++) {
+        if (bin[i] === '1') {
+          bn += Math.cos(n * val.pi_4 * i) - Math.cos(n * val.pi_4 * (i + 1));
+        }
+      }
+      return bn *= 1 / (n * val.pi);
+    },
+    _fourier: function(a0, bin, armn) {
+      var fn, points, self, x, _ref, _ref2;
+      self = this;
+      points = [];
+      fn = function(t) {
+        var f, n;
+        f = 0;
+        for (n = 1; 1 <= armn ? n <= armn : n >= armn; 1 <= armn ? n++ : n--) {
+          f += self._getan(bin, n) * Math.cos(val.w0 * n * t) + self._getbn(bin, n) * Math.sin(val.w0 * n * t);
+        }
+        return f += a0;
+      };
+      for (x = 0, _ref = val.T * 8, _ref2 = val.T / 50; 0 <= _ref ? x <= _ref : x >= _ref; x += _ref2) {
+        points.push([x, fn(x)]);
+      }
+      return $.plot(dom.graph, [points]);
+    }
+  };
 
-$(function () {
-	node.init();
-});
+  $(function() {
+    return node.init();
+  });
+
+}).call(this);
